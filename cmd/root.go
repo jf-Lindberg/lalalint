4fab7a6a54fe9ce66247ab15a5e0e8518c9077fb
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2022-09-15 11:02:47 +0200

    type: structure
    subject: heavily modified the root command to have an architecture that
    relies more on subcommands rather than arguments. the root command is
    now simply the starting point of the application instead of being a jack
    of all trades.

--- cmd/root.go
+++ cmd/root.go
@@ -5,46 +5,47 @@ package cmd
 
 import (
 	"fmt"
-	"github.com/jf-Lindberg/lalalint/pkg/reader"
-	"github.com/jf-Lindberg/lalalint/pkg/validate"
+	cc "github.com/ivanpirog/coloredcobra"
+	"github.com/jf-Lindberg/lalalint/helper"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"os"
 )
 
 var (
-	cfgFile    string
-	outputFile string
+	cfgFile   string
+	inputFile string
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "lalalint <filename> [flags]",
+	Use:   "lalalint",
 	Short: "Lalalint is a LaTeX linter for .tex files. Basic usage: lalalint <file>.",
 	Long:  `Lalalint is a LaTeX linter for .tex files.`,
-	Args:  cobra.ExactArgs(1),
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	RunE: func(cmd *cobra.Command, args []string) error {
-		fileName := args[0]
-		err := validate.FileName(fileName)
-		if err != nil {
-			return err
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		if cmd.PersistentFlags().Lookup("errors").Changed {
+			viper.Set("errors", true)
 		}
 
-		reader.Reader(fileName)
-
-		/*		output := cmd.Flags().Lookup("write").Value
-				fmt.Println(output)
-				fmt.Println("GetString:", viper.GetString("write"))*/
-
 		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		err := cmd.Help()
+		helper.LogFatal(err)
 	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	cc.Init(&cc.Config{
+		RootCmd:  rootCmd,
+		Headings: cc.HiCyan + cc.Bold + cc.Underline,
+		Commands: cc.HiYellow + cc.Bold,
+		Example:  cc.Italic,
+		ExecName: cc.Bold,
+		Flags:    cc.Bold,
+	})
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
@@ -53,36 +54,19 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "Config file (default is $HOME/.lalalint.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().StringVarP(&outputFile, "write", "w", "", "Name of output file ending with .tex")
-	rootCmd.Flags().BoolP("overwrite", "o", false, "Overwrites input file")
+	rootCmd.PersistentFlags().BoolP("errors", "e", false, "prints errors (default is off except for the check command)")
+	viper.BindPFlag("showErrors", rootCmd.PersistentFlags().Lookup("errors"))
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
-		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".lalalint" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
+		viper.AddConfigPath(".")
+		viper.SetConfigType("json")
 		viper.SetConfigName(".lalalint")
 	}
-
-	viper.BindPFlag("write", rootCmd.Flags().Lookup("write"))
 
 	viper.AutomaticEnv() // read in environment variables that match
 
