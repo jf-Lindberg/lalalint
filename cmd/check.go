6c010c98019f31689192453a134710d709fc09bd
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2022-09-15 11:07:26 +0200

    type: feature
    subject: implemented a "check" command that can check a file for errors.
    prints all errors found by default (configurable). this might be a
    placeholder feature depending on feedback from Michael. if he likes it,
    an idea is to allow multiple file names as arguments in order to check
    multiple files at a time

---
+++ cmd/check.go
@@ -0,0 +1,41 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"github.com/jf-Lindberg/lalalint/lalalint"
+	"github.com/jf-Lindberg/lalalint/validate"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// checkCmd represents the check command
+var checkCmd = &cobra.Command{
+	Use:   "check",
+	Short: "Outputs all of the linter errors found to the terminal.",
+	Long:  `longer description`,
+	Args:  cobra.MinimumNArgs(1),
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		for i := range args {
+			err := validate.FileName(args[i])
+			if err != nil {
+				return err
+			}
+		}
+		rootCmd.PersistentFlags().Lookup("showErrors")
+		viper.Set("showErrors", viper.GetBool("commands.check.showErrors"))
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		for i := range args {
+			lalalint.Check(args[i])
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(checkCmd)
+}
