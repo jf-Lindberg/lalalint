e716b354dccd5d2ab16041142afd10ede5d349ca
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2022-09-24 16:57:57 +0200

    type: func
    subject: split announce function into two, added a func that compares
    types and returns a bool

--- helper/helper.go
+++ helper/helper.go
@@ -4,19 +4,30 @@ import (
 	"fmt"
 	"github.com/fatih/color"
 	"log"
+	"reflect"
 )
+
+func AnnounceDone(announcement string) {
+	color.Set(color.FgHiGreen, color.Bold)
+	fmt.Println(announcement)
+	color.Unset()
+}
+
+func AnnounceStart(announcement string) {
+	color.Set(color.FgHiCyan, color.Bold)
+	fmt.Println(announcement)
+	color.Unset()
+}
+
+func CompareType(a, b interface{}) bool {
+	return reflect.TypeOf(a) == reflect.TypeOf(b)
+}
 
 func LogFatal(e error) {
 	if e != nil {
 		color.Set(color.Bold, color.FgHiRed)
 		log.Fatal(e)
 	}
-}
-
-func Announce(announcement string) {
-	color.Set(color.FgHiWhite, color.Bold)
-	fmt.Println(announcement)
-	color.Unset()
 }
 
 func PrintLintErr(err error) {
