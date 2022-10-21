590336bbba78184c11cfa90b08034685a29dd8dc
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2022-10-21 20:50:19 +0200

    type: refactoring
    subject: rule now relies on regex

--- linter/space_after_comment.go
+++ linter/space_after_comment.go
@@ -19,33 +19,6 @@ func (e BadCommentError) Error() string {
 	return fmt.Sprintf("no space after comment on row %d, character %d: '%s'", e.Row, e.Index+1, e.Fault)
 }
 
-///*
-//SpaceAfterComment Checks if comment is formatted correctly, meaning that there should be a space after valid comments.
-//Valid comments meaning non-escaped ones such as "\% this is an escaped comment".
-//*/
-//func SpaceAfterComment(line Line, symbol string) (Line, error) {
-//	// Regex: .*%[^\s] or maybe even better (%)([^\s]), then just add whitespace between CG1 and CG2
-//	// Checks that line includes %
-//	if index := commentIndex(line.Content, symbol); index != -1 {
-//		// First checks if index is 0, then checks that previous character is not the escape character.
-//		// This check is in place to avoid index out of range errors and to not format comments that are escaped.
-//		if index == 0 || !commentEscaped(index, line.Content) {
-//			// If comment is correct, leave and return whole line
-//			if !commentIsCorrect(index, line.Content) {
-//				// Else, return corrected line
-//				fault := GetFault(index, line)
-//				return correctLine(index, line), BadCommentError{line.Row, index, fault}
-//			}
-//		}
-//	}
-//
-//	// Return unchanged line
-//	return line, nil
-//}
-
-/*
-commentEscaped determines whether the LaTeX escape character was used before the comment symbol
-*/
 func commentEscaped(index int, lineContent string) bool {
 	if index != 0 {
 		return lineContent[index-1:index] == "\\"
@@ -53,13 +26,8 @@ func commentEscaped(index int, lineContent string) bool {
 	return false
 }
 
-//func commentIndex(line Line) []int {
-//	regex := invalidCommentRegex()
-//	return regex.FindIndex([]byte(line.Content))
-//}
-
 func invalidCommentRegex() *regexp.Regexp {
-	r := fmt.Sprintf("(?P<symbol>%s)(?P<character>%s)", "%", `[^\s]`)
+	r := fmt.Sprintf("(?P<symbol>%s)(?P<character>%s)", "%", `[^\s"']`)
 	return regexp.MustCompile(r)
 }
 
@@ -77,32 +45,3 @@ func SpaceAfterComment(line Line) (Line, error) {
 
 	return line, nil
 }
-
-//func FindComment(symbol string, content string) int {
-//	r := fmt.Sprintf("(?P<symbol>%s(?P<character>%s)", symbol, `[^\s]`)
-//	pattern := regexp.MustCompile(r)
-//	match := pattern.FindIndex([]byte(content))
-//	return
-//}
-
-///*
-//commentIndex returns the index of substring
-//*/
-//func commentIndex(lineContent string, char string) int {
-//	return strings.Index(lineContent, char)
-//}
-
-///*
-//commentIsCorrect determines whether there is a space after the comment symbol
-//*/
-//func commentIsCorrect(index int, lineContent string) bool {
-//	return lineContent[index+1:index+2] == " "
-//}
-
-///*
-//correctLine corrects the line to the proper format (adds space after the %)
-//*/
-//func correctLine(index int, line Line) Line {
-//	content := line.Content[:index+1] + " " + line.Content[index+1:]
-//	return Line{line.Row, content}
-//}
