5354127560e63e18a6407a177715f1844c274011
Filip Lindberg <9118820+jf-Lindberg@users.noreply.github.com>
2022-10-21 20:51:49 +0200

    added .bib and .tikz to validated file names

--- validate/file_name.go
+++ validate/file_name.go
@@ -18,7 +18,7 @@ func (e BadFileNameError) Error() string {
 }
 
 func FileName(fileName string) error {
-	if found, _ := regexp.MatchString("(.+.)(.tex|.bib|.tikz)$", fileName); found != true {
+	if found, _ := regexp.MatchString("(.+.)(.tex|.bib|.tikz)$", fileName); !found {
 		return BadFileNameError{fileName}
 	}
 
